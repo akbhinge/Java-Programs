f622a58b937c8f8c819ceb40444bfdfff8f8daed
akbhinge <abhinge@gmail.com>
2017-06-28 12:52:37 -0700

    - Added code for the function to find Minimum value from rotated array

--- src/javaPracticeProgs/arrayRotate.java
+++ src/javaPracticeProgs/arrayRotate.java
@@ -4,14 +4,14 @@ import java.util.Scanner;
 
 public class arrayRotate {
 
-    public void Rotate(int[] nums, int k){
+    private void Rotate(int[] nums, int k){
 
         reverse(nums, 0,nums.length-1);
         reverse(nums,0, k-1);
         reverse(nums,k,nums.length-1);
     }
 
-    public void reverse(int[] nums1, int l, int r){
+    private void reverse(int[] nums1, int l, int r){
 
         while(l <r){
 
@@ -21,6 +21,32 @@ public class arrayRotate {
             l++;
             r--;
         }
+    }
+
+    private int findMin(int[] nums){
+        if(nums.length == 0){
+            return 0;
+        }
+        if(nums.length == 1){
+            return nums[0];
+        }
+
+        int i=0,j=nums.length-1;
+
+        while(i<j){
+            int mid = i + (j-i) /2;
+
+            if(mid >0 && nums[mid] < nums[mid-1]){
+                return nums[mid];
+            }
+            if(nums[i] <= nums[mid] && nums[mid] > nums[j]){
+                i = mid +1;
+            }
+            else{
+                j = mid - 1;
+            }
+        }
+        return nums[i];
     }
 
     public static void main(String[] args){
@@ -50,5 +76,9 @@ public class arrayRotate {
         for(int element: arr){
             System.out.print(element + " ");
         }
+        System.out.println();
+        System.out.println("Min element from rotated array: " + ar1.findMin(arr));
+
+        in.close();
     }
 }
